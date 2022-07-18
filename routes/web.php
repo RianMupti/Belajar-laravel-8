6353d7ada7fcd404e2edb5c403f7fd5e0387dabc
Rian Mupti Jauhari <rianmupti@gmail.com>
2022-07-18 07:09:13 +0700

    Belajar struktur folder, routes & view

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Rian Mupti Jauhari",
+        "email" => "rianmupti@gmail.com",
+        "image" => "coba.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
