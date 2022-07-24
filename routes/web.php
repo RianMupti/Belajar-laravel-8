a4f363cf49fb2cfe18dd243d719bdc183d8611b8
Rian Mupti Jauhari <rianmupti@gmail.com>
2022-07-24 11:36:44 +0700

    belajar Model, Collection, & Controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -33,54 +36,7 @@ Route::get('/about', function () {
     ]);
 });
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Rian Mupti Jauhari",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil earum debitis blanditiis qui veritatis numquam corrupti minima ipsam aperiam. Nobis quis impedit quibusdam? Placeat est eum totam, laboriosam iste fugit ratione et deserunt quod? Excepturi commodi fuga nesciunt dolorum dolorem, similique quis culpa qui deserunt velit quisquam. Beatae dolorem porro excepturi veniam amet ducimus aliquid, expedita doloremque iste repellendus nam saepe deserunt non rem vel, eaque nesciunt cum quos veritatis labore. Ullam nostrum doloribus impedit animi amet. Velit minima similique fuga omnis. Quo quam enim maiores eum aspernatur, incidunt doloribus!"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Yusup Maulana",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil earum debitis blanditiis qui veritatis numquam corrupti minima ipsam aperiam. Nobis quis impedit quibusdam? Placeat est eum totam, laboriosam iste fugit ratione et deserunt quod? Excepturi commodi fuga nesciunt dolorum dolorem, similique quis culpa qui deserunt velit quisquam. Beatae dolorem porro excepturi veniam amet ducimus aliquid, expedita doloremque iste repellendus nam saepe deserunt non rem vel, eaque nesciunt cum quos veritatis labore. Ullam nostrum doloribus impedit animi amet. Velit minima similique fuga omnis. Quo quam enim maiores eum aspernatur, incidunt doloribus!"
-        ]
-    ];
-    
-    return view('posts', [
-        "title" => "posts",
-        "posts" => $blog_posts
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
 
 // Halaman single post
-Route::get('posts/{slug}', function ($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Rian Mupti Jauhari",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil earum debitis blanditiis qui veritatis numquam corrupti minima ipsam aperiam. Nobis quis impedit quibusdam? Placeat est eum totam, laboriosam iste fugit ratione et deserunt quod? Excepturi commodi fuga nesciunt dolorum dolorem, similique quis culpa qui deserunt velit quisquam. Beatae dolorem porro excepturi veniam amet ducimus aliquid, expedita doloremque iste repellendus nam saepe deserunt non rem vel, eaque nesciunt cum quos veritatis labore. Ullam nostrum doloribus impedit animi amet. Velit minima similique fuga omnis. Quo quam enim maiores eum aspernatur, incidunt doloribus!"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Yusup Maulana",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil earum debitis blanditiis qui veritatis numquam corrupti minima ipsam aperiam. Nobis quis impedit quibusdam? Placeat est eum totam, laboriosam iste fugit ratione et deserunt quod? Excepturi commodi fuga nesciunt dolorum dolorem, similique quis culpa qui deserunt velit quisquam. Beatae dolorem porro excepturi veniam amet ducimus aliquid, expedita doloremque iste repellendus nam saepe deserunt non rem vel, eaque nesciunt cum quos veritatis labore. Ullam nostrum doloribus impedit animi amet. Velit minima similique fuga omnis. Quo quam enim maiores eum aspernatur, incidunt doloribus!"
-        ]
-    ];
-
-    $new_post = [];
-    foreach($blog_posts as $post) {
-        if($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('posts/{slug}', [PostController::class, 'show']);
