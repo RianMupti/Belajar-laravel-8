d7fb7fc3ebfb44bdfc4472a9f1722807e7248a55
Rian Mupti Jauhari <rianmupti@gmail.com>
2022-07-28 06:36:11 +0700

    belajar git seeder

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,7 +4,7 @@
     <article>
         <h2>{{ $post->title }}</h2>
 
-        <p>By. Rian Mupti Jauhari in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+        <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
         
         <p>{!! $post->body !!}</p>
     </article>
